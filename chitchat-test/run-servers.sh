eaae4e65045e9c9003d860ef6e61611bb83868cf
T <taimoor.imtiaz@intel.com>
2022-05-06 12:59:00 +0200

    Update run-servers.sh
    Use the `-h` command line arg instead of `--host`

--- chitchat-test/run-servers.sh
+++ chitchat-test/run-servers.sh
@@ -4,7 +4,7 @@ for i in $(seq 10000 10100)
 do
     listen_addr="localhost:$i";
     echo ${listen_addr};
-    ./target/release/chitchat-test --host ${listen_addr} --seed localhost:10050&
+    ./target/release/chitchat-test -h ${listen_addr} --seed localhost:10050&
 done;
 
 read
